7e4d3fa6ad2499f52ad2f9e5c5493db280263321
locphan90 <124541721+locphan90@users.noreply.github.com>
2025-01-27 10:33:36 +0700

    Update index.js 3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { MongoClient, ObjectId } = require("mongodb");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = 10000;
+const port = 3000;
 
 // Middleware
 app.use(bodyParser.json());
